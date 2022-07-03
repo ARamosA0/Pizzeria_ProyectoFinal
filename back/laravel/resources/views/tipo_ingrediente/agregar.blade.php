0c248422cce86a48cf9b2729045ae4fbf247bd16
Anthonella Rua <anthonella.rua@tecsup.edu.pe>
2022-07-02 23:21:11 -0500

    crud terminado

--- back/laravel/resources/views/tipo_ingrediente/agregar.blade.php
+++ back/laravel/resources/views/tipo_ingrediente/agregar.blade.php
@@ -8,9 +8,12 @@
             <input type="text" class="form-control" id="tipo_id" name="tipo_id" value="{{$tipo_id}}">
         </div>
         <div class="mb-3">
-            <label class="form-label">ID DEL INGREDIENTE</label>
-            <input type="text" class="form-control" id="ingrediente_id" name="ingrediente_id">
+            <label class="form-label">INGREDIENTES PARA AGREGAR</label><br>
+            @foreach($ingredientes as $ingrediente)
+                <input type="checkbox" id="ingrediente_id" name="ingredientes[]" value={{$ingrediente->_id}}>
+                <label for="ingrediente_id">{{$ingrediente->nombre}}</label><br>
+            @endforeach
         </div>
-        <button type="submit" class="btn btn-secondary">Guardar ingrediente</button>
+        <button type="submit" name="enviar" value="enviar" class="btn btn-secondary">Guardar ingrediente</button>
     </form>
 @endsection
